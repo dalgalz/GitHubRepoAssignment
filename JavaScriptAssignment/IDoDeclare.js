e292557e97c926f7e05cb3ea5812664f5f89b7d2
Duangdao <patavanich.d@gmail.com>
2017-08-10 20:32:45 -0700

    Add JS Assignment

---
+++ JavaScriptAssignment/IDoDeclare.js
@@ -0,0 +1,21 @@
+var num1 = 234
+var num2 = -452
+var num3 = 0.28
+var num4 = 0
+var string1 = "Glad"
+var string2 = "Danny"
+var string3 = "Harry"
+var isTrue = true
+var isFalse = false
+var what = undefined
+
+console.log("This is the First Number", num1)
+console.log("This is the Second Number", num2)
+console.log("This is the Third Number", num3)
+console.log("This is the forth Number", num4)
+console.log("This is the First String", string1)
+console.log("This is the Second String", string2)
+console.log("This is the Third String", string3)
+console.log("This is True", isTrue)
+console.log("This is False", isFalse)
+console.log("This is Undefined", what)
