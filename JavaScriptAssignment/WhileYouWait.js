41cd2806dfa6f829878fcf0a14e80fa05a1e7e5d
Duangdao <patavanich.d@gmail.com>
2017-08-12 00:28:53 -0700

    Add WhileYouWait Assignment

---
+++ JavaScriptAssignment/WhileYouWait.js
@@ -0,0 +1,21 @@
+var daysUntilMyBirthday = 60;
+
+while( daysUntilMyBirthday > 0 )
+{
+  if( daysUntilMyBirthday < 5 )
+  {
+    console.log( daysUntilMyBirthday + " DAYS UNTIL MY BIRTHDAY....OMG!!!")
+  }
+  else if( daysUntilMyBirthday < 30)
+  {
+    console.log( daysUntilMyBirthday + " days until my birthday....So Excited!!!")
+  }
+  else if( daysUntilMyBirthday >= 30 )
+  {
+    console.log( daysUntilMyBirthday + " days until my birthday....It's been forever T-T")
+  }
+
+  daysUntilMyBirthday--;
+}
+
+console.log("It's My Birthday!!! Happy Birthday to me :)")
