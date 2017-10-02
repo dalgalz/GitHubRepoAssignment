806a1b4d5b5bce58d2a91cc49ac04b453ddeaa0b
Duangdao <patavanich.d@gmail.com>
2017-10-02 12:22:47 -0700

    Fixed Ninja Gold

--- Flask/NinjaGold/server.py
+++ Flask/NinjaGold/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, session
+from datetime import datetime
 import random
 
 app = Flask(__name__)
@@ -11,33 +12,34 @@ def index():
 	# The random module has many useful functions. This is one that gives a random number in a range
 	if "coin" not in session: 
 		session["coin"] = 0;
+		session['act'] = []
   	return render_template("index.html")
 
 @app.route('/process_money', methods=['POST'])
 def guess_num():
-	if "act" not in session: 
-		session["act"] = "";
+	time = datetime.now().strftime("%Y/%m/%d %I:%M:%S %p")
+
 	# The random module has many useful functions. This is one that gives a random number in a range
 	building = request.form['building']
 	if building == "farm":
 		randomNum = random.randrange(10, 21)
 		session["coin"] += randomNum;
-		session["act"] += "<p class=\'earn\'>Earn " + str(randomNum) + " goals from " + building + "!</p><br>"
+		session["act"].append({'activity':"Earned {} golds".format(randomNum), 'class':'earn', 'date':time})
 	elif building == "cave":
 		randomNum = random.randrange(5, 11)
 		session["coin"] += randomNum;
-		session["act"] += "<p class='earn'>Earn " + str(randomNum) + " goals from " + building + "!</p><br>"
+		session["act"].append({'activity':"Earned {} golds".format(randomNum), 'class':'earn', 'date':time})
 	elif building == "house":
 		randomNum = random.randrange(2, 6)
 		session["coin"] += randomNum;
-		session["act"] += "<p class='earn'>Earn " + str(randomNum) + " goals from " + building + "!</p><br>"
+		session["act"].append({'activity':"Earned {} golds".format(randomNum), 'class':'earn', 'date':time})
 	elif building == "casino":
 		randomNum = random.randrange(-50, 51)
 		session["coin"] += randomNum;
 		if randomNum >= 0:
-			session["act"] += "<p class='earn'>Earn " + str(randomNum) + " goals from " + building + "!</p><br>"
+			session["act"].append({'activity':"Earned {} golds".format(randomNum), 'class':'earn', 'date':time})
 		elif randomNum < 0:
-			session["act"] += "<p class='earn'>Lose " + str(abs(randomNum)) + " goals from " + building + "!</p><br>"
+			session["act"].append({'activity':"Lose {} golds".format(abs(randomNum)), 'class':'lose', 'date':time})
   	return redirect('/')
 
 app.run(debug=True) # run our server
