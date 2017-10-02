a9356f836d550a2799e7de84594499c630cdc709
Duangdao <patavanich.d@gmail.com>
2017-10-02 13:38:21 -0700

    Add Validation

--- Flask/Dojo_Survey/server.py
+++ Flask/Dojo_Survey/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 app = Flask(__name__)
+app.secret_key = 'KeepItSecretKeepItSafe'
 # our index route will handle rendering our form
 @app.route('/')
 def index():
@@ -16,6 +17,13 @@ def create_user():
    lang = request.form['lang']
    comment = request.form['comment']
    # redirects back to the '/' route
+   if len(name) < 1:
+      flash("Name cannot be empty!") # just pass a string to the flash function
+   if len(comment) < 1:
+      print "Hey"
+      flash("comment cannot be empty!") # just pass a string to the flash function
+   if len(comment) > 120:
+      flash("Limit only 120 characters") # just pass a string to the flash function
    return render_template("result.html" , name=name, location=location, lang=lang, comment=comment)
 
 app.run(debug=True) # run our server
