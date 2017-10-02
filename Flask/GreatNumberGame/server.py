5c78340f30f7d3ce41f994a6a7458c08f0a8ac52
Duangdao <patavanich.d@gmail.com>
2017-10-01 23:46:25 -0700

    Try Great Number Game Assignment

---
+++ Flask/GreatNumberGame/server.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, session
+import random
+
+app = Flask(__name__)
+app.secret_key = 'ThisIsSecret' # you need to set a secret key for security purposes
+# routing rules and rest of server.py below
+# our index route will handle rendering our form
+
+@app.route('/')
+def index():
+	# The random module has many useful functions. This is one that gives a random number in a range
+	if "randNum" not in session: 
+		session['randNum'] = random.randrange(0, 101) # random number between 0-100
+  	print session['randNum']
+  	return render_template("index.html")
+
+@app.route('/guess', methods=['POST'])
+def guess_num():
+	# The random module has many useful functions. This is one that gives a random number in a range
+	guessNum = request.form['guess']
+	print guessNum
+	print session['randNum']
+	if int(guessNum) < session['randNum']:
+		session['hint'] = "Too Low!!!"
+	elif int(guessNum) > session['randNum']:
+		session['hint'] = "Too High!!!"
+	elif int(guessNum) == session['randNum']:
+		session['correct'] = session['randNum'], "was the number!!!"
+		session.pop('randNum')
+		session.pop('hint')
+  	return redirect('/')
+
+app.run(debug=True) # run our server
