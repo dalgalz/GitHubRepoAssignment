e40594715251197e7af5e728b9f526ebc9d514bb
Duangdao <patavanich.d@gmail.com>
2017-10-03 17:28:26 -0700

    fixed countries.sql

--- MySQL/Countries/countries.sql
+++ MySQL/Countries/countries.sql
@@ -1,15 +1,49 @@
 #1. What query would you run to get all the countries that speak Slovene?
 #Your query should return the name of the country, language and language percentage.
 #Your query should arrange the result by language percentage in descending order. (1)
-#select * from countries;
-#select * from languages;
-#select countries.name, languages.language, languages.percentage from countries join languages
-#on countries.code = languages.country_code where language = 'Slovene' order by percentage desc;
+select countries.name, languages.language, languages.percentage from countries join languages
+on countries.code = languages.country_code where language = 'Slovene' order by percentage desc;
 
 #2. What query would you run to display the total number of cities for each country?
 #Your query should return the name of the country and the total number of cities.
 #Your query should arrange the result by the number of cities in descending order. (3)
-select countries.code, SUM(cities.name) as TotalOfCities from countries join cities
-on countries.code = cities.country_code
-group by countries.code order by countries.code desc limit 100;
-#select * from cities;
+select countries.name, COUNT(cities.id) as TotalOfCities from countries join cities
+on countries.id = cities.country_id
+group by countries.id order by COUNT(cities.id) desc;
+
+#3. What query would you run to get all the cities in Mexico with a population of greater than 500,000?
+#Your query should arrange the result by population in descending order. (1)
+Select cities.name, cities.population from countries join cities on countries.id = cities.country_id
+where countries.name = 'Mexico' and cities.population > 500000
+order by cities.population desc; 
+
+#4. What query would you run to get all languages in each country with a percentage greater than 89%?
+#Your query should arrange the result by percentage in descending order. (1)
+select countries.name, languages.language, languages.percentage from countries join languages
+on countries.id = languages.country_id where languages.percentage > 89 order by languages.percentage;
+
+#5. What query would you run to get all the countries with Surface Area below 501 and Population greater
+#than 100,000? (2)
+select countries.name, countries.surface_area, countries.population from countries
+where surface_area < 501 and population > 100000; 
+
+#6. What query would you run to get countries with only Constitutional Monarchy with a capital greater 
+#than 200 and a life expectancy greater than 75 years? (1)
+select countries.name, countries.government_form, countries.capital from countries
+where countries.government_form = 'Constitutional Monarchy'
+and countries.life_expectancy > 75 and countries.capital > 200;
+
+#7. What query would you run to get all the cities of Argentina inside the Buenos Aires district and have
+#the population greater than 500, 000? The query should return the Country Name, City Name, District and Population. (2)
+select countries.name, cities.name, cities.district, cities.population
+from countries join cities ON countries.id = cities.country_id
+where countries.name = 'Argentina'
+and cities.district = 'Buenos Aires'
+and cities.population > 500000;
+    
+#8. What query would you run to summarize the number of countries in each region?
+#The query should display the name of the region and the number of countries. Also, the query should arrange the result by the number of countries in descending order. (2)
+select countries.region, count(countries.id) as num_countries
+from countries
+group by countries.region
+order by num_countries DESC;
